b65663a399548ea7fcdf342d885deb9be2265e0f
jeeyun <jeeyunryu@gmail.com>
2023-05-10 21:36:49 +0900

    edit files

--- src/penny/screens/donation.js
+++ src/penny/screens/donation.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import {Typography } from "@mui/material";
+import {Typography, Card } from "@mui/material";
 
 import user from "../data/users";
 
@@ -16,37 +16,78 @@ user.map((u) => {
 export default function App() {
   return (
     <div>
-        <Grid container spacing={1}>
-            <Grid item xs={5}>
-                <Button><ArrowBackIosIcon/></Button>
-            </Grid>
-            <Grid item xs={5}>
+      <div style={{maringTop: '10px'}}>
+        <Grid container spacing={1} >
+          <Grid item xs={5} style={{marginTop: '10px'}}>
+            <Button style={{color: 'black'}}><ArrowBackIosIcon/></Button>
+          </Grid>
+          <Grid item xs={5} style={{marginTop: '17px', marginBottom: '0px'}}>
             {user.map((u) => (
-            <Typography variant="h5" gutterBottom>
-              {u.name}
-            </Typography>
-          ))}
-            </Grid>
-            <Grid item xs={2}>
-                <Button>취소</Button>
-            </Grid>
-            
+              <Typography variant="h5" gutterBottom style={{marginBottom: '0px'}}>
+                {u.name}
+              </Typography>
+            ))}
+          </Grid>
+          <Grid item xs={2} style={{marginTop: '10px'}}>
+            <Button style={{color: 'black', paddingRight: '15px'}}>취소</Button>
+          </Grid>   
         </Grid>
-        <p style={{textAlign: "center"}}>{accountInfo.length > 0 && accountInfo[0].bank} {accountInfo.length > 0 && accountInfo[0].number}</p>
-        <br/>
-        <h1 style={{textAlign: "center"}}>421원</h1>
-        <br/>
-        
-        <p style={{textAlign: "center"}}>잔액: {accountInfo.length > 0 && accountInfo[0].balance}</p>
-        <br/>
-        <p style={{textAlign: "center"}}>모금 후 예상 잔액: {accountInfo.length > 0 && accountInfo[0].balance-421}</p>
-        <br/>
-        <p style={{textAlign: "center"}}>위의 금액을 모금하시겠습니까?</p>
-        <br/>
-        <Button fullWidth variant="contained" style={{backgroundColor: 'yellow', color: 'black'}}>확인</Button>
-        
-
-
+      </div>
+      <p style={{textAlign: "center", marginTop: '0px', fontSize: '13px', color: 'gray'}}>{accountInfo.length > 0 && accountInfo[0].bank} {accountInfo.length > 0 && accountInfo[0].number}</p>
+      <br/>
+      <h1 style={{textAlign: "center", marginTop: '50px'}}>{accountInfo.length > 0 && accountInfo[0].balance%1000}원</h1>
+      <br/>
+      <Card style={{
+        marginTop: '20px',
+        width: '300px',
+        height: '50px',
+        backgroundColor: '#F5F5F5',
+        borderRadius: '5px', // added border radius
+        margin: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'left',
+        paddingLeft: '10px'
+        }
+      }>
+        잔액: {accountInfo.length > 0 && accountInfo[0].balance}원
+      </Card>
+      <Card style={{
+        marginTop: '20px',
+        width: '300px',
+        height: '50px',
+        backgroundColor: 'lightGray',
+        borderRadius: '5px', // added border radius
+        margin: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'left',
+        paddingLeft: '10px',
+        marginBottom: '50px'
+        }}
+      >
+        모금 후 예상 잔액: {accountInfo.length > 0 && accountInfo[0].balance-(accountInfo[0].balance%1000)}원
+      </Card>
+      <p style={{textAlign: "center"}}>위의 금액을 모금하시겠습니까?</p>
+      <br/>
+      <div style={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+        <Button variant="contained" 
+          style={{ 
+            backgroundColor: '#F7E676', 
+            color: 'black' , 
+            height: '46px', 
+            width: '350px', 
+            borderRadius: 0, 
+            marginBottom: '100px', 
+            marginLeft: '10px', 
+            marginRight: '10px',
+            boxShadow: 'none',
+            borderRadius: '5px',
+          }}
+        >
+          확인
+        </Button>
+      </div>
     </div>
   );
 }
--- src/penny/screens/homeAuto.js
+++ src/penny/screens/homeAuto.js
@@ -15,103 +15,157 @@ user.map((u) => {
   u.account.map((acc, index) => (accountInfo[index] = acc));
 });
 
-
 export default function App() {
   return (
     <div style={{
-      backgroundColor: 'gray'
+      backgroundColor: 'gray', minHeight: '100%'
     }}>
-      <Grid container spacing={1}>
-        <Grid item xs={2}>
-        {user.map((u) => (
-            <Typography variant="h5" gutterBottom>
+      <Grid container spacing={1} style={{marginBottom: '20px'}}>
+        <Grid item xs={3} style={{marginTop: '20px'}}>
+          {user.map((u) => (
+            <Typography variant="h5" gutterBottom sx={{ marginLeft: '10px' }}>
               {u.name}
             </Typography>
           ))}
-          
         </Grid>
-        <Grid item xs={8}>
-          
+        <Grid item xs={7} style={{marginTop: '20px'}}>
           <Chip label="내 계좌" />
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} style={{marginTop: '20px'}}>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         </Grid>
       </Grid>
 
-      <Card>
-        <div>
-          <p>
-            10만명 X 100원 = "3억"
-          </p>
-          <p>티끌의 힘!</p>
-        </div>
-      </Card>
-
-
-      <Card>
-        <Grid container spacing={3} style={{ backgroundColor:'pink'}}>
-            <Grid item xs={2}>
-              <Avatar>B</Avatar>
-            </Grid>
-            <Grid item xs={8}>
-              잔액
-            </Grid>
-            <Grid item xs={2}>
-              <Button><MoreHorizIcon/></Button>
-            </Grid>
-              <Grid item >
-                {accountInfo.length > 0 && accountInfo[0].balance}원
-              </Grid>
-              <Grid item>
-                <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-                  카드
-                </Button>
-                <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-                  이체
-                </Button>
-                <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-                  수동
-                </Button>
-              </Grid>
-          </Grid>
-        </Card>
-      
-      <Card>
-        <Grid container spacing={3} style={{ backgroundColor:'lightblue'}}>
-          <Grid item xs={2}>
-            <Avatar>B</Avatar>
-          </Grid>
-          <Grid item xs={8}>
-            Penny
-          </Grid>
-          <Grid item xs={2}>
-            <Button><SettingsIcon/></Button>
-          </Grid>
-          
-          <Grid item >
-            {user.map((u) => (
+      <Card style={{backgroundColor: '#EBC5C8',
+        marginTop: '10px',
+        marginLeft: '10px',
+        marginRight: '10px',
+        marginBottom: '5px'
+      }}>
+      <Grid container spacing={3} style={{padding: '16px'}}>
+        <Grid item xs={10}>
+          잔액
+        </Grid>
+        <Grid item xs={2}>
+          <Button sx={{ color: '#c88a8a' }} style={{paddingRight: '20px'}}><MoreHorizIcon/></Button>
+        </Grid>
+        <Grid item>
+          {accountInfo.length > 0 && accountInfo[0].balance}원
+        </Grid>
+        <Grid item justifyContent="flex-start">
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              mr: 1,
+              backgroundColor: '#c88a8a', // darker pink color
+              color: '#fff', // white text color
+              boxShadow: 'none' // remove shadow
+            }}
+          >
+            카드
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              mr: 1,
+              backgroundColor: '#c88a8a', // darker pink color
+              color: '#fff', // white text color
+              boxShadow: 'none' // remove shadow
+            }}
+          >
+            이체
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              marginLeft: 'auto',
+              backgroundColor: '#c88a8a', // darker pink color
+              color: '#fff', // white text color
+              boxShadow: 'none' // remove shadow
+            }}
+          >
+            수동
+          </Button>
+        </Grid>
+      </Grid>
+    </Card>
+    <Card style={{backgroundColor: '#BFDDFB',
+      marginTop: '10px',
+      marginLeft: '10px',
+      marginRight: '10px',
+      marginBottom: '5px'
+    }}>
+      <Grid container spacing={3} style={{padding: '16px'}}>
+        <Grid item xs={10}>
+          Penny
+        </Grid>
+        <Grid item xs={2}>
+          <Button sx={{ color: '#7FB4D4' }} style={{paddingRight: '20px'}}><SettingsIcon/></Button>
+        </Grid>
+        <Grid item>
+          {user.map((u) => (
             <Typography>
               {u.currentDonationAmount}원
             </Typography>
           ))}
-            </Grid>       
-          
-          <Grid item>
-            <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-              개인모금
-            </Button>
-            <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-              자동모금
-            </Button>
-            <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-              내역
-            </Button>
-          </Grid>
         </Grid>
-      </Card>
-
-      
-    </div>
+        <Grid item sx={{ display: 'flex', justifyContent: 'flex-start' }} xs={12}>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              mr: 1,
+              marginLeft: 'auto',
+              backgroundColor: '#7FB4D4', // darker pink color
+              color: '#fff', // white text color
+              boxShadow: 'none' // remove shadow
+            }}
+          >
+            개인모금
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              mr: 1,
+              marginLeft: '0px',
+              backgroundColor: '#7FB4D4', // darker pink color
+              color: '#fff', // white text color
+              boxShadow: 'none' // remove shadow
+            }}
+          >
+            함꼐모금
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              marginLeft: '0px',
+              backgroundColor: '#7FB4D4', // darker pink color
+              color: '#fff', // white text color
+              boxShadow: 'none' // remove shadow
+            }}
+          >
+            내역
+          </Button>
+        </Grid>
+      </Grid>
+    </Card>
+  </div>
   );
 }
